9ddab9284f73cd23d5bce7c59f8e88f8d54f3031
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-09-27 22:00:37 +0900

    commit

--- 구조체/TextGame.c
+++ 구조체/TextGame.c
@@ -1,47 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-struct M4_Owner
-{
-
-	int _hp;
-	int _damage;
-	int _defence;
-};
-typedef struct M4_Owner M4_Owner;
-
-struct AK47_Owner
-{
-
-	int _hp;
-	int _damage;
-	int _defence;
-};
-typedef struct AK47_Owner AK47_Owner;
-
-struct Kar98_Owner
-{
-
-	int _hp;
-	int _damage;
-	int _defence;
-};
-typedef struct Kar98_Owner Kar98_Owner;
-
-struct Scar_Owner
-{
-
-	int _hp;
-	int _damage;
-	int _defence;
-};
-typedef struct Scar_Owner Scar_Owner;
-
-// 배그말고 한 게임
+// Player 타입
+#define M4_Owner 1
+#define AK47_Owner 2
+#define Kar98_Owner 3
+#define Scar_Owner 4
 
 
+// Monster 타입
+#define EasyEnemy 5
+#define NormalEnemy 6
+#define StringEnemy 7
+
+// Player 스탯
 struct PlayerInfo
 {
 	int _hp;
@@ -51,6 +26,7 @@ struct PlayerInfo
 };
 typedef struct PlayerInfo PlayerInfo;
 
+// 적군 스탯
 struct EnemyInfo
 {
 	int _hp;
@@ -61,45 +37,77 @@ typedef struct EnemyInfo EnemyInfo;
 
 void CreatePlayer(PlayerInfo* player)
 {
-	printf("플레이어가 생성되었습니다!!\n");
-	player->_hp = 300;
-	player->_damage = 20;
-	player->_defence = 10;
-}
-void CreateEnemy(EnemyInfo* Enemy)
-{
-	printf("적군이 생성되었습니다!!\n");
-	Enemy->_hp = 200;
-	Enemy->_damage = 10;
-	Enemy->_defence = 5;
+	int selectPlayer;
+	while (1)
+	{
+		printf("1. M4\n2. AK-47\n3. Kar98\n4. Scar\n");
+		printf("플레이어를 선택해주세요 : ");
+		scanf("%d", &selectPlayer);
+		switch (selectPlayer)
+		{
+		case M4_Owner:
+			player->_hp = 200;
+			player->_damage = 20;
+			player->_defence = 5;
+			printf("당신이 선택한 총기는 M4입니다\n");
+			return;
+		case AK47_Owner:
+			player->_hp = 200;
+			player->_damage = 30;
+			player->_defence = 5;
+			printf("당신이 선택한 총기는 AK47입니다\n");
+			return;
+		case Kar98_Owner:
+			player->_hp = 100;
+			player->_damage = 50;
+			player->_defence = 0;
+			printf("당신이 선택한 총기는 Kar98입니다\n");
+			return;
+		case Scar_Owner:
+			player->_hp = 200;
+			player->_damage = 15;
+			player->_defence = 10;
+			printf("당신이 선택한 총기는 Scar입니다\n");
+			return;
+		default:
+			printf("\n플레이어를 잘못 선택하셨습니다 다시 선택해주세요!\n\n");
+			break;
+		}
+	}
 }
 
-void PrintPlayerInfo(PlayerInfo* player)
+void EnterBattleArea(PlayerInfo* player)
 {
-	printf("\n\n============플레이어 정보============\n");
-	printf("플레이어의 체력 : %d\n", player->_hp);
-	printf("플레이어의 공격력 : %d\n", player->_damage);
-	printf("플레이어의 방어력 : %d\n", player->_defence);
-}
-void PrintEnemyInfo(EnemyInfo* enemy)
-{
-	printf("\n\n============적군 정보============\n");
-	printf("적군의 체력: %d\n", enemy->_hp);
-	printf("적군의 공격력 : %d\n", enemy->_damage);
-	printf("적군의 방어력 : %d\n", enemy->_defence);
+	while (1)
+	{
+		printf("===================\n");
+		printf("전장에 진입했습니다!\n");
+		printf("===================\n\n");
+
+		printf("===================\n");
+		printf("1. 전투 참여 2. 도주\n");
+		printf("===================\n\n");
+		int select;
+		scanf("%d", &select);
+		if (select == 1)
+		{
+			// TODO 
+			// CreateEnemy();
+			// BattleStart();
+		}
+		else
+		{
+			printf("전장에서 이탈했습니다!!\n");
+			return;
+		}
+
+	}
 }
 
 int main()
 {
-
+	srand((unsigned int)time(NULL));
 	PlayerInfo* player = (PlayerInfo*)malloc(sizeof(PlayerInfo));
 	CreatePlayer(player);
-	EnemyInfo* enemy = (EnemyInfo*)malloc(sizeof(EnemyInfo));
-	CreateEnemy(enemy);
-	PrintPlayerInfo(player);
-	PrintEnemyInfo(enemy);
-	
-	
-
-
+	EnterBattleArea(player);
 }
