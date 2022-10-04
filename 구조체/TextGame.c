d58a0bad423c47c2587b77a3f81ca8a6086ba5e9
Windfall-Online <109807780+Windfall-Online@users.noreply.github.com>
2022-10-04 21:59:22 +0900

    commit

--- 구조체/TextGame.c
+++ 구조체/TextGame.c
@@ -10,11 +10,10 @@
 #define Kar98_Owner 3
 #define Scar_Owner 4
 
-
 // Monster 타입
 #define EasyEnemy 5
 #define NormalEnemy 6
-#define StringEnemy 7
+#define StrongEnemy 7
 
 // Player 스탯
 struct PlayerInfo
@@ -32,8 +31,11 @@ struct EnemyInfo
 	int _hp;
 	int _damage;
 	int _defence;
+	int _type;
 };
 typedef struct EnemyInfo EnemyInfo;
+
+void BattleStart(PlayerInfo* player, EnemyInfo* enemy);
 
 void CreatePlayer(PlayerInfo* player)
 {
@@ -46,25 +48,25 @@ void CreatePlayer(PlayerInfo* player)
 		switch (selectPlayer)
 		{
 		case M4_Owner:
-			player->_hp = 200;
+			player->_hp = 500;
 			player->_damage = 20;
 			player->_defence = 5;
 			printf("당신이 선택한 총기는 M4입니다\n");
 			return;
 		case AK47_Owner:
-			player->_hp = 200;
+			player->_hp = 500;
 			player->_damage = 30;
 			player->_defence = 5;
 			printf("당신이 선택한 총기는 AK47입니다\n");
 			return;
 		case Kar98_Owner:
-			player->_hp = 100;
+			player->_hp = 500;
 			player->_damage = 50;
 			player->_defence = 0;
 			printf("당신이 선택한 총기는 Kar98입니다\n");
 			return;
 		case Scar_Owner:
-			player->_hp = 200;
+			player->_hp = 500;
 			player->_damage = 15;
 			player->_defence = 10;
 			printf("당신이 선택한 총기는 Scar입니다\n");
@@ -73,6 +75,113 @@ void CreatePlayer(PlayerInfo* player)
 			printf("\n플레이어를 잘못 선택하셨습니다 다시 선택해주세요!\n\n");
 			break;
 		}
+
+	}
+}
+
+void CreateEnemy(PlayerInfo* player)
+{
+	EnemyInfo* enemy = (EnemyInfo*)malloc(sizeof(EnemyInfo));
+	enemy->_type = (rand() % 3) + 5; // (0, 1, 2) + 5
+
+	switch (enemy->_type)
+	{
+	case EasyEnemy:
+		enemy->_hp = 200;
+		enemy->_defence = 1;
+		enemy->_damage = 20;
+		break;
+	case NormalEnemy:
+		enemy->_hp = 300;
+		enemy->_defence = 5;
+		enemy->_damage = 50;
+		break;
+	case StrongEnemy:
+		enemy->_hp = 1000;
+		enemy->_defence = 10;
+		enemy->_damage = 80;
+		break;
+	}
+
+	BattleStart(player, enemy);
+}
+
+void BattleStart(PlayerInfo* player, EnemyInfo* enemy)
+{
+	while (1)
+	{
+		printf("\n\n");
+		printf("나의 현재 체력 : %d\n", player->_hp);
+		printf("적의 현재 체력 : %d\n", enemy->_hp);
+		// 먼저 공격!!
+		int damage = player->_damage - enemy->_defence;
+		// 내 공격력이 10이고 상대 방어력이 5면 원래 10들어갈거 5가 까이고 들어감
+		int ShitAim = rand() % 2;
+		if (ShitAim == 1)
+		{
+			printf("으악 에임이 구렸다!\n");
+			printf("적의 현재 체력 : %d\n", enemy->_hp);
+		}
+		else
+		{
+			int bug = rand() % 100;
+			if (bug == 1)
+			{
+				printf("fucking galaxy\n");
+				exit(1);
+			}
+			if (damage < 0)
+			{
+				damage = 0;
+			}
+			printf("나의 공격 시작!\n");
+			printf("적에게 준 데미지 : %d\n", damage);
+			enemy->_hp -= damage;
+
+			if (enemy->_hp < 0)
+			{
+				enemy->_hp = 0;
+			}
+			printf("enemy의 남은 체력 : %d\n", enemy->_hp);
+			if (enemy->_hp == 0)
+			{
+				printf("enemy를 처치했습니다!! 축하합니다!!\n");
+				return;
+			}
+		}
+
+		// 안죽었으면 내가 맞을 차례다
+		damage = enemy->_damage - player->_defence;
+		if (damage < 0)
+		{
+			damage = 0;
+		}
+		int ShitAim2 = rand() % 2;
+	
+		if (ShitAim2 == 1)
+		{
+			printf("적군의 공격이 빗나갔다!\n");
+			printf("나의 현재 체력 : %d\n", player->_hp);
+		}
+		else
+		{
+			printf("적의 공격 시작!\n");
+			printf("내가 받은 데미지 : %d\n", damage);
+			player->_hp -= damage;
+
+			if (player->_hp < 0)
+			{
+				player->_hp = 0;
+			}
+			printf("player의 남은 체력 : %d\n", player->_hp);
+			if (player->_hp == 0)
+			{
+				printf("You Died\n");
+				return;
+			}
+			printf("턴 종료\n\n");
+		}
+	
 	}
 }
 
@@ -80,6 +189,7 @@ void EnterBattleArea(PlayerInfo* player)
 {
 	while (1)
 	{
+
 		printf("===================\n");
 		printf("전장에 진입했습니다!\n");
 		printf("===================\n\n");
@@ -91,9 +201,15 @@ void EnterBattleArea(PlayerInfo* player)
 		scanf("%d", &select);
 		if (select == 1)
 		{
-			// TODO 
-			// CreateEnemy();
-			// BattleStart();
+			CreateEnemy(player);
+			if (player->_hp == 0)
+			{
+				return;
+			}
+			else
+			{
+				return;
+			}
 		}
 		else
 		{
@@ -111,3 +227,7 @@ int main()
 	CreatePlayer(player);
 	EnterBattleArea(player);
 }
+
+// 포인터 변수 크기
+// 32bit - 4byte
+// 64bit - 8byte
---
+++ 구조체/궁금증해결.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+void ChangeNum(int value, int value2)
+{
+
+	// TODO
+	// 
+	// 숫자가오면 바꾼다
+	// 뭘로? 5로
+	
+}
+// value = testPtr = &bb
+int main()
+{
+	int bb = 100;
+	int* testPtr = NULL;
+	testPtr = &bb;
+	int cc = 10;
+	//testPtr = 0x000000CCD74FFB64'
+	ChangeNum(1000, 5000);
+	return 0;
+	// vs 2019 - 32bit 2의32승 비트까지 양을 전달할 수 있다
+	// 42? 4GB
+	//         - 64bit
+	// 2의 64승까지 무제한 수치도 근삿값
+	// 왜 C를 처음배우냐 
+	// ㅈㄴ구리니까
+	// 이게 되네?
+	// 파이썬으로 처음 시작?
+	// 얘는 너무 편함
+	// 이런 똥쓰레기 언어가 또 있나
+
+
+}
